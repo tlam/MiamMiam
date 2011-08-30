854db4d01265ada0993024a78c07151208ecaf77
Thierry Lam <lamthierry@gmail.com>
2011-08-29 23:57:32 -0400

    Added basic swipe left or right listener #6

--- src/com/tlam/miam/FoodDetail.java
+++ src/com/tlam/miam/FoodDetail.java
@@ -6,6 +6,10 @@ import android.database.Cursor;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.GestureDetector;
+import android.view.GestureDetector.SimpleOnGestureListener;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,6 +17,11 @@ import android.widget.TextView;
 public class FoodDetail extends Activity {
 
     private static final String TAG = "FoodDetail";
+    private static final int SWIPE_MIN_DISTANCE = 120;
+    private static final int SWIPE_MAX_OFF_PATH = 250;
+    private static final int SWIPE_THRESHOLD_VELOCITY = 200;
+
+    private GestureDetector gestureDetector;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -50,6 +59,39 @@ public class FoodDetail extends Activity {
 
         c.close();
         db.close();
+
+        gestureDetector = new GestureDetector(new MyGestureDetector());
+
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gestureDetector.onTouchEvent(event)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    private class MyGestureDetector extends SimpleOnGestureListener {
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            try {
+                if (Math.abs(e1.getY() - e2.getY()) > SWIPE_MAX_OFF_PATH)
+                    return false;
+                // right to left swipe
+                if(e1.getX() - e2.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    //Toast.makeText(SelectFilterActivity.this, "Left Swipe", Toast.LENGTH_SHORT).show();
+                    Log.i("FoodDetail", "Left Swipe");
+                }  else if (e2.getX() - e1.getX() > SWIPE_MIN_DISTANCE && Math.abs(velocityX) > SWIPE_THRESHOLD_VELOCITY) {
+                    //Toast.makeText(SelectFilterActivity.this, "Right Swipe", Toast.LENGTH_SHORT).show();
+                    Log.i("FoodDetail", "Right Swipe");
+                }
+            } catch (Exception e) {
+                // nothing
+            }
+            return false;
+        }
+    }
 }
